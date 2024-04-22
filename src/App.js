9bf789f09145551109953194001c06573a95535b
Bjork Weenk <bjorkweenk.bw@gmail.com>
2024-04-22 21:40:33 +0200

    do it

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-      <h1> Portfolio coming soon... </h1>
+      <h1> Portfolio coming soon </h1>
       </header>
     </div>
   );
