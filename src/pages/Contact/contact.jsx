d1a4a4e85e3c20aa9a8db4a5a7f4c469bd91c5a3
Bjork Weenk <bjorkweenk.bw@gmail.com>
2024-04-22 19:47:46 +0200

    components

---
+++ src/pages/Contact/contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Contact.css'
+
+
+const Contact = () => {
+    return (
+        <>
+           <h1> Contact </h1>
+        </>
+    )
+}
+
+export default Contact
---
+++ src/pages/Work/work.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Work.css'
+
+
+const Work = () => {
+    return (
+        <>
+           <h1> Work</h1>
+        </>
+    )
+}
+
+export default Work
